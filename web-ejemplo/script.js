cf029264807db4f543aecd324ca429e4a3708322
AndresVescovi <andresvescovi95@gmail.com>
2022-08-03 09:03:21 -0300

    firrst commit

---
+++ web-ejemplo/script.js
@@ -0,0 +1,25 @@
+const list = []
+const anotherList = list.concat(157)
+
+const persona = {
+    name : 'andres',
+    twiter: '@andres',
+    age: 18,
+    isDev: false,
+    liks: ['https://midu.tube','https://midu.tube']
+}
+
+const sumar = (operando1, operando2) =>{
+    console.log(operando1)
+    console.log(operando2)
+    return operando1 + operando2
+}
+
+function restar (a, b){
+    return a - b
+}
+
+op1 = 2
+op2 = 2
+
+console.log(sumar(op1, op2))
