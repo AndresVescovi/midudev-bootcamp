59b61cc3a0b7f36030b3dbd10faafb176908da63
AndresVescovi <andresvescovi95@gmail.com>
2022-08-03 09:02:13 -0300

    firts commit

---
+++ part1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Mensaje from './Mensaje';
+
+function App() {
+ return (
+    <div className="App">
+  <Mensaje color ='red' msj = "estamos trabajando en un cuso de react"/>
+    </div>
+  );
+}
+
+export default App;
