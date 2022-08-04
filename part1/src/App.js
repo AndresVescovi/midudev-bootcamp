6d21fa9472d46562488a0a894aa7a31eb44ba27d
AndresVescovi <andresvescovi95@gmail.com>
2022-08-04 09:01:28 -0300

    first commit

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import Mensaje from './Mensaje';
 
 function App() {
- return (
+  return (
     <div className="App">
-  <Mensaje color ='red' msj = "estamos trabajando en un cuso de react"/>
+      <Mensaje color ='red' msj = "estamos trabajando en un cuso de react"/>
     </div>
   );
 }
