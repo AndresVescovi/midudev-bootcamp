e9678994855d7b0b352907cc06ef1d13bcc8ddae
AndresVescovi <andresvescovi95@gmail.com>
2022-08-05 09:28:00 -0300

    preparacion clase 3

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Mensaje from './Mensaje';
+
 
 function App() {
   return (
     <div className="App">
-      <Mensaje color ='red' msj = "estamos trabajando en un cuso de react"/>
+      
     </div>
   );
 }
