ae0a1ffe88e85b695ac9ff83b64052839b52d518
AndresVescovi <andresvescovi95@gmail.com>
2022-08-08 18:50:41 -0300

    counter terminado

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hellow codesandbox</h1>
+      <h2> Start editing to see some magic hapend!</h2>
     </div>
   );
 }
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from 'react';
+
+/* incrementar = () =>{
+  console.log("click")
+} */
+
+const Counter = ({contador})=>{
+ return <h1> {contador} </h1>
+}
+
+const App = (props) => {
+
+  const [contador, setContador] = useState(0)
+  
+  console.log("render")
+
+const handleClick = (value)=>{
+value? setContador(contador + 1) : setContador(contador - 1)
+}
+
+/* const handleClickAdd = () =>{
+  setContador(contador + 1)
+}
+const handleClickSubtract = () =>{
+  setContador(contador - 1)
+} */
+const handleClickRestart = () =>{
+  setContador(0)
+}
+
+
+const isEven = contador % 2 === 0
+
+/* const isEven = () =>{
+  let even = contador % 2
+  if(even === 0){
+    return "el contador es par"
+  }else{
+    return "el contador es impar"
+  }
+} */
+
+  return (
+    <div>
+    <p>El valor del contador es:</p>
+    <Counter contador={contador}/>
+    <p>{isEven? 'Es par': 'Es impar'}</p>
+    <button onClick={()=> handleClick(true)}>incrementar</button>
+    <button onClick={handleClickRestart}>resetear</button>
+    <button onClick={()=>handleClick(false)}>disminuir</button>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <App/>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
